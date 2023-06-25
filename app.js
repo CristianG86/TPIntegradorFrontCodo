c6d2e5b765734108daebbe7be483d445c5a993e8
Cristian Guillen <guillencristian86@hotmail.com>
2023-06-25 19:53:41 -0300

    Tercer Commit

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-let valorTicket = 200;
+let valueTicket = 200;
 let alertEl = document.getElementById("alert-el")
 let alertDiv = document.getElementById("alertcont")
-document.getElementById("inputcantidad").value = 1
+document.getElementById("inputamount").value = 1
 
-function resumen() {
-    let cantidadEl = Math.ceil(document.getElementById("inputcantidad").value)
-    let categoriaEl = document.getElementById("categoria-el").value
-    document.getElementById("inputcantidad").value = cantidadEl 
+function success(){
+    Swal.fire({ 
+    title: '¡Excelente!',
+    text: '¡Verás el resumen a pagar!',
+    icon: 'succes',
+})}
 
-    if (cantidadEl < 0) {
-        cantidadEl = 1
-        document.getElementById("inputcantidad").value = cantidadEl 
-    } else if (cantidadEl > 10) {
-        cantidadEl = 10
-        document.getElementById("inputcantidad").value = cantidadEl 
+function summary() {
+    let amountEl = Math.ceil(document.getElementById("inputamount").value)
+    let categoryEl = document.getElementById("categoria-el").value
+    document.getElementById("inputamount").value = amountEl 
+
+    if (amountEl < 0) {
+        amountEl = 1
+        document.getElementById("inputamount").value = amountEl 
+    } else if (amountEl > 10) {
+        amountEl = 10
+        document.getElementById("inputamount").value = amountEl 
         var tag = document.createElement("div")
         tag.classList.add('alert-danger')
         tag.classList.add('alert')
@@ -22,31 +29,37 @@ function resumen() {
         alertDiv.appendChild(tag);
     }
 
-    if (categoriaEl == "estudiante") {
-        let totalAPagar = valorTicket * cantidadEl
-        totalAPagar = totalAPagar * 0.20
+    if (categoryEl == "estudiante") {
+        let totalPay = valueTicket * amountEl
+        totalPay = totalPay * 0.20
         alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
-    } else if (categoriaEl == "trainee") {
-        let totalAPagar = valorTicket * cantidadEl
-        totalAPagar = totalAPagar * 0.50
+        alertEl.innerText = alertEl.innerText + " " + totalPay
+        success()
+    } else if (categoryEl == "trainee") {
+        let totalPay = valueTicket * amountEl
+        totalPay = totalPay * 0.50
         alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
-    } else if (categoriaEl == "junior") {
-        let totalAPagar = valorTicket * cantidadEl
-        totalAPagar = totalAPagar * 0.85
+        alertEl.innerText = alertEl.innerText + " " + totalPay
+        success()
+    } else if (categoryEl == "junior") {
+        let totalPay = valueTicket * amountEl
+        totalPay = totalPay * 0.85
         alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
+        alertEl.innerText = alertEl.innerText + " " + totalPay
+        success()
     } else {
-        let totalAPagar = valorTicket * cantidadEl
+        let totalPay = valueTicket * amountEl
         alertEl.innerText = "Total a Pagar: $"
-        alertEl.innerText = alertEl.innerText + " " + totalAPagar
+        alertEl.innerText = alertEl.innerText + " " + totalPay
+        success()
     }
 }
 
-function borrar() {
-    let cantidadEl = Math.ceil(document.getElementById("inputcantidad").value)
+
+
+function erase() {
+    let cantidadEl = Math.ceil(document.getElementById("inputamount").value)
     let categoriaEl = document.getElementById("categoria-el").value
-    document.getElementById("inputcantidad").value = 1
+    document.getElementById("inputamount").value = 1
     alertEl.innerText = "Total a Pagar: $"
 }
